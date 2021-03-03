ab20a6e552bffde2e11386b26d75157d2ac0719b
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-03-03 13:05:51 +0100

    Query string parameters

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Querystring from 'query-string'
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
+  const result = Querystring.parse(location.search);
+  console.log(result);
   return (
     <div>
       <h1>Posts</h1>
